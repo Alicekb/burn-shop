f6af22b72f501334740d3011d0e7c13a68d58cdf
alicekb <alicekb01@gmail.com>
2020-09-08 13:26:45 -0400

    Setup color pallete in emotion theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "emotion-theming";
+
+const theme = {
+  colors: {
+    burnt: "#f24405",
+    purple: "#220E32",
+    orange: "#f28705",
+    yellow: "#f2cb05",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
