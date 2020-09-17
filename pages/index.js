57025c5d8e6a9c5bc768403e2fda31ef16379eb8
alicekb <alicekb01@gmail.com>
2020-09-17 19:35:36 -0400

    Clean up repeating graphl fields into fragments

--- pages/index.js
+++ pages/index.js
@@ -20,37 +20,44 @@ const MainArea = styled.div`
   grid-template-columns: 1fr 1fr;
 `;
 
+const IndexPage = {
+  fragments: {
+    items: gql`
+      fragment IndexPageItems on items {
+        id
+        cloud_filename
+        cost
+        name
+      }
+    `,
+  },
+};
+
 const GET_RECENT_ITEMS = gql`
   query getMyRecentItems {
-    items(order_by: { created_at: desc }, limit: 20) {
-      id
-      cloud_filename
-      cost
-      name
+    items(order_by: { created_at: desc }, limit: 30) {
+      ...IndexPageItems
     }
   }
+  ${IndexPage.fragments.items}
 `;
 
 const GET_ANNOUNCED_ITEMS = gql`
   query getAnnouncedItems {
-    items(order_by: { updated_at: desc }, limit: 30, offset: 30) {
-      id
-      cloud_filename
-      cost
-      name
+    items(order_by: { updated_at: desc }, limit: 30) {
+      ...IndexPageItems
     }
   }
+  ${IndexPage.fragments.items}
 `;
 
 const GET_FEATURED_ITEMS = gql`
   query getFeaturedItems {
     items(order_by: { cost: desc }, limit: 30) {
-      id
-      cloud_filename
-      cost
-      name
+      ...IndexPageItems
     }
   }
+  ${IndexPage.fragments.items}
 `;
 
 const Home = () => {
