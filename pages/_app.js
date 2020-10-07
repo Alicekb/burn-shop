dc4b0a8c0c75ea594ba91fcc7bc7448cdee9e6f0
alicekb <alicekb01@gmail.com>
2020-10-07 13:40:32 -0400

    Add nprogress for loading bar and spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import { ApolloProvider } from "@apollo/client";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
 import { useApollo } from "../lib/apolloClient";
-import "../styles/globals.css";
 import { ThemeProvider } from "emotion-theming";
+import "../styles/globals.css";
+import "../styles/nprogress.css";
 
 const theme = {
   colors: {
@@ -16,6 +20,27 @@ const theme = {
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => NProgress.start();
+    const handleRouteChangeComplete = (url) => NProgress.done();
+    const handleRouteChangeError = (err, url) => {
+      if (err.cancelled) {
+        NProgress.done();
+      }
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeError);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeError);
+    };
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
